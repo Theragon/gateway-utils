38b19b8c3a39174570d8a13438f541f2638f05f3
logi <logantheragon@gmail.com>
2016-03-17 23:40:01 +0000

    added wrapper functions for different http requests with different headers

--- httputils.py
+++ httputils.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
+import requests
+import json
 
 #from enum import Enum
+
+timeout = None
 
 
 class MimeType():
@@ -9,6 +13,7 @@ class MimeType():
 	app_json = 'application/json'
 	app_yaml = 'application/x-yaml'
 	text_yaml = 'text/yaml'
+	app_oct = 'application/octet-stream'
 
 # HTTP Headers
 #app_xml = 'application/xml'
@@ -24,6 +29,11 @@ class Headers():
 	APP_JSON = {'Content-Type': MimeType.app_json, 'Accept': MimeType.app_json}
 	APP_YAML = {'Content-Type': MimeType.app_yaml, 'Accept': MimeType.app_yaml}
 	TEXT_YAML = {'Content-Type': MimeType.text_yaml, 'Accept': MimeType.text_yaml}
+	APP_OCT = \
+		{
+			'Accept': MimeType.app_json + ',' + MimeType.app_oct,
+			'Content-Type': MimeType.app_oct
+		}
 
 #APP_XML = {'Content-Type': MimeType.app_xml, 'Accept': MimeType.app_xml}
 #TEXT_XML = {'Content-Type': MimeType.text_xml, 'Accept': MimeType.text_xml}
@@ -65,6 +75,60 @@ def is_xml(mimetype):
 
 def is_yaml(mimetype):
 	return mimetype == MimeType.app_yaml or mimetype == MimeType.text_yaml
+
+
+def post_req_json(url, data, to=timeout):
+	"""
+	Wrapper function to post json requests
+	"""
+	try:
+		json_data = json.dumps(data)
+		r = requests.post(url, data=json_data, headers=Headers.app_json, timeout=to)
+		return r
+	except requests.exceptions.Timeout as e:
+		return e
+	except ValueError as e:
+		return e
+
+
+def put_req_json(url, data, to=timeout):
+	"""
+	Wrapper function to put json requests
+	"""
+	try:
+		json_data = json.dumps(data)
+		r = requests.put(url, data=json_data, headers=Headers.app_json, timeout=to)
+		return r
+	except requests.exceptions.Timeout as e:
+		return e
+	except ValueError as e:
+		return e
+
+
+def post_req_xml(url, data, to=timeout):
+	"""
+	Wrapper function to post json requests
+	"""
+	try:
+		r = requests.post(url, data=data, headers=Headers.app_xml, timeout=to)
+		return r
+	except requests.exceptions.Timeout as e:
+		return e
+	except ValueError as e:
+		return e
+
+
+def post_req_oct(url, data, to=timeout):
+	"""
+	Wrapper function to post json requests
+	"""
+	try:
+		r = requests.post(url, data=data, headers=Headers.app_oct, timeout=to)
+		return r
+	except requests.exceptions.Timeout as e:
+		return e
+	except ValueError as e:
+		return e
 
 
 class GatewayTimeoutException(Exception):
